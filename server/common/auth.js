2a3f8ca8e2e0784dd7759c17df669a0fa556c4b3
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-02-03 15:25:41 -0500

    Cache userProfile info by accessToken

--- server/common/auth.js
+++ server/common/auth.js
@@ -1,6 +1,10 @@
 import jwt from 'express-jwt';
 import jwksRsa from 'jwks-rsa';
+import NodeCache from 'node-cache';
 import { AuthenticationClient } from 'auth0';
+import l from './logger';
+
+const myCache = new NodeCache({ stdTTL: 3600, checkperiod: 240 });
 
 const auth0 = new AuthenticationClient({
   domain: 'login.deephire.com',
@@ -24,8 +28,14 @@ export const checkJwt = jwt({
 });
 
 export async function getEmail(accessToken) {
-  auth0.getProfile(accessToken);
-  const { email } = await auth0.getProfile(accessToken);
-  return email;
+  const value = myCache.get(accessToken);
+  if (value === undefined) {
+    auth0.getProfile(accessToken);
+    const { email } = await auth0.getProfile(accessToken);
+    myCache.set(accessToken, email);
+    l.info(`set ${email} in cache`);
+    return email;
+  }
+  l.info(`got ${value} from cache`);
+  return value;
 }
-
