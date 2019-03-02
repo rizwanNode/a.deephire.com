1a76624c2249d53da7068c8ccdcc9e2bf5200f87
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-03-02 06:19:27 -0500

    interview controller from class to functions

--- server/api/controllers/interviews/controller.js
+++ server/api/controllers/interviews/controller.js
@@ -1,30 +1,22 @@
 import InterviewsService from '../../services/interviews.service';
 
-export class Controller {
-  all(req, res) {
-    InterviewsService.all(res.locals.email).then(r => res.json(r));
-  }
+export const all = (req, res) => {
+  InterviewsService.all(res.locals.email).then(r => res.json(r));
+};
 
-  insert(req, res) {
-    InterviewsService.insert(req.body, res.locals.email).then(r => {
-      res
-        .status(201)
-        .location(`/v1/interviews/${r._id}`)
-        .json(r);
-    });
-  }
+export const insert = (req, res) => {
+  InterviewsService.insert(req.body, res.locals.email).then(r => res.status(r).end());
+};
 
-  byParam(req, res) {
-    InterviewsService.byParam(req.params.id).then(r => {
-      if (r) res.json(r);
-      else res.status(500).end();
-    });
-  }
+export const byParam = (req, res) => {
+  InterviewsService.byParam(req.params.id).then(r => {
+    if (r) res.json(r);
+    else res.status(500).end();
+  });
+};
 
-  delete(req, res) {
-    InterviewsService.delete(req.params.id).then(r => {
-      res.status(r).end();
-    });
-  }
-}
-export default new Controller();
+export const deleteData = (req, res) => {
+  InterviewsService.delete(req.params.id).then(r => {
+    res.status(r).end();
+  });
+};
--- server/api/controllers/interviews/router.js
+++ server/api/controllers/interviews/router.js
@@ -1,12 +1,12 @@
 import * as express from 'express';
-import controller from './controller';
 import auth from '../../../common/auth';
+import { all, insert, byParam, deleteData } from './controller';
 
 
 export default express
   .Router()
-  .get('/', auth, controller.all)
-  .post('/', auth, controller.insert)
-  .get('/:id', controller.byParam)
-  .delete('/:id', auth, controller.delete);
+  .get('/', auth, all)
+  .post('/', auth, insert)
+  .get('/:id', byParam)
+  .delete('/:id', auth, deleteData);
 
