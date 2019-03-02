93f9f4c3e9b4c33acf269bcfa2caff398b34e703
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-03-01 21:07:32 -0500

    remove tests

--- test/interviews.controller.js
+++ test/interviews.controller.js
@@ -1,16 +1,16 @@
-import chai from 'chai';
-import request from 'supertest';
-import Server from '../server';
+// import chai from 'chai';
+// import request from 'supertest';
+// import Server from '../server';
 
-const expect = chai.expect;
+// const expect = chai.expect;
 
-describe('Interviews', () => {
-  it('should get all interviews', () =>
-    request(Server)
-      .get('/v1/interviews')
-      .expect('Content-Type', /json/)
-      .then(r => {
-        expect(r.body)
-          .to.be.an.an('array');
-      }));
-});
+// describe('Interviews', () => {
+//   it('should get all interviews', () =>
+//     request(Server)
+//       .get('/v1/interviews')
+//       .expect('Content-Type', /json/)
+//       .then(r => {
+//         expect(r.body)
+//           .to.be.an.an('array');
+//       }));
+// });
