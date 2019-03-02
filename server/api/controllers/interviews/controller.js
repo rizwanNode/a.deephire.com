fa6bbacceb55889033fd04201b22025e2a03eeff
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-03-01 21:36:39 -0500

    add GET POST Interviews

--- server/api/controllers/interviews/controller.js
+++ server/api/controllers/interviews/controller.js
@@ -5,6 +5,22 @@ export class Controller {
     InterviewsService.all(res.locals.email).then(r => res.json(r));
   }
 
+  insert(req, res) {
+    InterviewsService.insert(req.body, res.locals.email).then(r => {
+      res
+        .status(201)
+        .location(`/v1/interviews/${r._id}`)
+        .json(r);
+    });
+  }
+
+  byParam(req, res) {
+    InterviewsService.byParam(req.params.id).then(r => {
+      if (r) res.json(r);
+      else res.status(500).end();
+    });
+  }
+
   delete(req, res) {
     InterviewsService.delete(req.params.id).then(r => {
       res.status(r).end();
--- server/api/controllers/interviews/router.js
+++ server/api/controllers/interviews/router.js
@@ -1,8 +1,12 @@
 import * as express from 'express';
 import controller from './controller';
+import auth from '../../../common/auth';
+
 
 export default express
   .Router()
-  .get('/', controller.all)
-  .delete('/:id', controller.delete);
+  .get('/', auth, controller.all)
+  .post('/', auth, controller.insert)
+  .get('/:id', controller.byParam)
+  .delete('/:id', auth, controller.delete);
 
