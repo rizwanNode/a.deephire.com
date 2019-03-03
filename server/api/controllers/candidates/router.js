90192a538e5502f403c4438fa26e1bd2b8a18588
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-03-03 07:43:45 -0500

    remove auth for Interviews route

--- server/api/controllers/candidates/router.js
+++ server/api/controllers/candidates/router.js
@@ -6,7 +6,7 @@ import auth from '../../../common/auth';
 
 export default express
   .Router()
-  .get('/:userId/', controller.byParam)
+  .get('/:userId', controller.byParam)
   .post('/:userId/documents/:objectKey', auth, controller.postDocuments)
   .get('/:userId/documents/:num', auth, controller.getDocuments)
   .put('/:userId', auth, controller.put);
