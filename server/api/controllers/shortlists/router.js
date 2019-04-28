47d7c3a5000a2f1c19c0f8d49571fc0a3ea72f80
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-04-28 17:29:00 -0400

    add auth to /archives routes

--- server/api/controllers/shortlists/router.js
+++ server/api/controllers/shortlists/router.js
@@ -4,7 +4,7 @@ import auth from '../../../common/auth';
 
 export default express
   .Router()
-  .get('/archives', controller.archives)
+  .get('/archives', auth, controller.archives)
   .post('/archive', auth, controller.archive)
   .post('/unarchive', auth, controller.unarchive)
   .get('/', auth, controller.all)
