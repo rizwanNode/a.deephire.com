0044dc61345eee205354166b030fb2bdb2cbe4a8
russell <russell@deephire.com>
2019-11-04 17:30:53 -0500

    fix logging for POST Companies

--- server/api/services/companies.service.js
+++ server/api/services/companies.service.js
@@ -3,7 +3,7 @@ import { byParam, insert } from './db.service';
 
 class CompaniesService {
   insert(data) {
-    l.info(`${this.constructor.name}.insert(${data})`);
+    l.info(`${this.constructor.name}.insert(${JSON.stringify(data)})`);
     return insert(data, 'companies').then(r => r._id);
   }
 
