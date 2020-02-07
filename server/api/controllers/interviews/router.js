95703b02891f4e28cc624b1146a9a00ef56ec3b0
russell <russell@deephire.com>
2020-02-07 12:48:39 -0600

    add candidate invites

--- server/api/controllers/interviews/router.js
+++ server/api/controllers/interviews/router.js
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import auth from '../../../common/auth';
-import { all, archive, archives, byParam, insert, unarchive, put, duplicate } from './controller';
+import { all, archive, archives, byParam, insert, unarchive, put, duplicate, invite } from './controller';
 
 export default express
   .Router()
@@ -8,6 +8,7 @@ export default express
   .post('/archive', auth, archive)
   .post('/unarchive', auth, unarchive)
   .post('/duplicate', auth, duplicate)
+  .post('/:id/invites', auth, invite)
 
   .get('/', auth, all)
   .post('/', auth, insert)
