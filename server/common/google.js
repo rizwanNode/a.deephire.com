8a1f160bc75295efa2579e7217cc32565d40cc8c
russell <russell@deephire.com>
2020-05-19 13:53:34 -0500

    add candidate name and jobName to calendar invite

--- server/common/google.js
+++ server/common/google.js
@@ -6,12 +6,12 @@ const clientEmail = process.env.GC_CLIENT_EMAIL;
 let privateKey = process.env.GC_PRIVATE_KEY;
 privateKey = privateKey.split('\\n').join('\n');
 
-const sendCalendarInvites = async (interviewLink, companyName, attendees, interviewTime) => {
+const sendCalendarInvites = async (interviewLink, companyName, attendees, interviewTime, candidateName, jobName) => {
   const [startTime, endTime] = interviewTime;
   const auth = new google.auth.JWT(clientEmail, null, privateKey, ['https://www.googleapis.com/auth/calendar'], 'interviews@deephire.com');
   const calendar = google.calendar({ version: 'v3', auth });
   const event = {
-    summary: `${companyName} Interview`,
+    summary: `${companyName} Interview, ${candidateName} ${jobName ? `, ${jobName}` : ''} `,
     start: {
       dateTime: startTime
     },
