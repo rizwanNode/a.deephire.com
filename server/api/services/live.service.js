d2172527727d25fc7aa5ede3cc5b59fa9e69512c
russell <russell@deephire.com>
2020-12-03 09:24:35 -0600

    appleoneTest company ID

--- server/api/services/live.service.js
+++ server/api/services/live.service.js
@@ -186,8 +186,8 @@ class LiveService {
     // eslint-disable-next-line camelcase
     if (
       userProfile?.user_id === 'auth0|5f7f2546ec8f030075525516' ||
-      companyId === '5f960ca1aaf3e97ca402a51d' ||
-      companyId === '5e95d7d3aed1120001480d69'
+      companyId === '5f960ca1aaf3e97ca402a51d' || companyId === '5e95d7d3aed1120001480d69' ||
+      companyId === '5f7f25460d77330001bc9b91' // appleoneTest company ID
     ) {
       interviewLink = `https://beta.live.deephire.com/room/${_id}`;
     }
